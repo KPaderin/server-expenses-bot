4b207928924eb27ad0d605bee4f63de618f1376f
Kirill <kirjaes@yandex.ru>
2023-02-11 00:02:54 +0500

    del useless files, consts

--- src/dataBase/db.js
+++ src/dataBase/db.js
@@ -1,5 +1,3 @@
-const mongoose = require('mongoose');
-
 const mongooseModel = require('./mogooseModel');
 
 const dbFunc = {
--- src/helpers/getNewId.js
+++
@@ -1,3 +0,0 @@
-const getNewId = ()  => Date.now().toString() + Math.floor(Math.random()*10000)
-
-module.exports = getNewId
